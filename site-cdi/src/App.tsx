b4ee333088435974eddbadc90fecb44ce318f3f7
Camila Lordelo <camilalordelo15@gmail.com>
2023-08-15 13:44:34 -0300

    Ajustando localização da Home

--- site-cdi/src/App.tsx
+++ site-cdi/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './Home';
+import Home from './pages/Home';
 import SobreNos from './pages/SobreNos';
 import Membros from './pages/Membros';
 import NossasIniciativas from './pages/NossasIniciativas';

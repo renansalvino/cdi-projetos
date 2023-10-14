499c48db13e5e2cdf6bd7e8d768467cc2f09df0f
Leonardo Rogelio <72422101+LeoRogelioSilva@users.noreply.github.com>
2023-10-14 01:05:55 -0300

    fixes

--- src/components/TitleHome.jsx
+++ src/components/TitleHome.jsx
@@ -6,16 +6,16 @@ function TitleHome() {
     return (
         <>
             <Box h="80vh" zIndex="-1">
-                <svg viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
-                    <path d="M0 35 Q20 20 40 30 T70 35 Q100 20 120 30 V50 T0 50 Z" />
+                <svg viewBox="0 0 100 150" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
+                    <path d="M0 35 Q20 20 40 30 T70 35 Q100 20 120 30 V150 T0 150 Z" />
                     <image x="65" y="10" xlinkHref="./img/pinguim.png" width="20" height="20" className="penguinImage" />
 
-                    <foreignObject x="5" y="6" width="90" height="45">
-                        <div class="textHeaderCover">
-                            <p style={{fontSize: '0.1rem' , fontWeight: 'bold', marginTop: '2px'}}>nós somos a</p>
-                            <h2 style={{fontSize: '0.2rem', fontWeight: 'bold'}}>Comissão discente de</h2>
-                            <h1 style={{fontSize: '0.3rem', fontWeight: 'bold'}}>INFORMÁTICA</h1>
-                            <p style={{fontSize: '0.1rem', fontWeight: 'bold'}}>da faculdade de tecnologia da Unicamp</p>
+                    <foreignObject x="5" y="6" width="90" height="100">
+                        <div style={{ fontWeight: 'bold', color: 'white', textAlign: 'left'}}>
+                            <p style={{fontSize: '0.1rem' , fontWeight: 'bold', margin: 0, color: 'white'}}>nós somos a</p>
+                            <h2 style={{fontSize: '0.2rem', fontWeight: 'bold', margin: 0, color: 'white'}}>Comissão discente de</h2>
+                            <h2 style={{fontSize: '0.3rem', fontWeight: 'bold', margin: 0, color: 'white'}}>INFORMÁTICA</h2>
+                            <p style={{fontSize: '0.1rem', fontWeight: 'bold', margin: 0, color: 'white'}}>da faculdade de tecnologia da Unicamp</p>
                         </div>
                     </foreignObject>
                 </svg>
